acca58221351ce9d19c447621aa523eb1f6d7f7a
Дмитрий Киселев <dmit-kiselyov3105@yandex.ru>
2022-06-14 22:10:53 +0300

    Sprint18.06

--- homework.py
+++ homework.py
@@ -1,27 +1,29 @@
 from dataclasses import dataclass, asdict
-from typing import ClassVar
+from typing import ClassVar, Dict, Type, List, Union
 
 
 @dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
+
     training_type: str
     duration: float
     distance: float
     speed: float
     calories: float
-    message = ('Тип тренировки: {training_type}; '
-               'Длительность:{duration: 0.3f} ч.; '
-               'Дистанция:{distance: 0.3f} км; '
-               'Ср. скорость:{speed: 0.3f} км/ч; '
-               'Потрачено ккал: {calories:0.3f}.')
+    Message: ClassVar[str] = ('Тип тренировки: {training_type}; '
+                              'Длительность: {duration:.3f} ч.; '
+                              'Дистанция: {distance:.3f} км; '
+                              'Ср. скорость: {speed:.3f} км/ч; '
+                              'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
-        return self.message.format(**asdict(self))
+        return self.Message.format(**asdict(self))
 
 
 class Training:
     """Базовый класс тренировки."""
+
     LEN_STEP: float = 0.65
     M_IN_KM: float = 1000
     MIN_IN_H: float = 60
@@ -49,40 +51,32 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        return InfoMessage(self.__class__.__name__,
-                           self.duration,
-                           distance,
-                           mean_speed,
-                           spent_calories)
+        return InfoMessage(training_type=self.__class__.__name__,
+                           duration=self.duration,
+                           distance=self.get_distance(),
+                           speed=self.get_mean_speed(),
+                           calories=self.get_spent_calories()
+                           )
 
 
 class Running(Training):
     """Тренировка: бег."""
-    coeff_calorie_1: float = 18
-    coeff_calorie_2: float = 20
 
-    def __init__(self,
-                 action: float,
-                 duration: float,
-                 weight: float
-                 ) -> None:
-        super().__init__(action, duration, weight)
+    RUN_COEFF_1: float = 18
+    RUN_COEFF_2: float = 20
 
     def get_spent_calories(self) -> float:
-        return ((self.coeff_calorie_1
-                * self.get_mean_speed()
-                - self.coeff_calorie_2)
-                * self.weight
-                / self.M_IN_KM
-                * (self.duration
-                * self.MIN_IN_H))
+        return ((self.RUN_COEFF_1 * self.get_mean_speed()
+                 - self.RUN_COEFF_2) * self.weight / self.M_IN_KM
+                * self.duration * self.MIN_IN_H)
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    coeff_walking_1: float = 0.035
-    coeff_walking_2: float = 2
-    coeff_walking_3: float = 0.029
+
+    WALK_COEFF_1: float = 0.035
+    WALK_COEFF_2: float = 0.029
+    WALK_COEFF_3: float = 2
 
     def __init__(self,
                  action: float,
@@ -94,21 +88,16 @@ class SportsWalking(Training):
         self.height = height
 
     def get_spent_calories(self) -> float:
-        return ((self.coeff_walking_1
-                * self.weight
-                + (self.get_mean_speed()
-                   ** self.coeff_walking_2
-                   // self.height)
-                * self.coeff_walking_3
-                * self.weight)
-                * (self.duration
-                * self.MIN_IN_H))
+        return ((self.WALK_COEFF_1 * self.weight
+                + (self.get_mean_speed()**self.WALK_COEFF_3 // self.height)
+                * self.WALK_COEFF_2 * self.weight)
+                * self.duration * self.MIN_IN_H)
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    coeff_swim_1: float = 1.1
-    coeff_swim_2: float = 2
+
+    SWIM_COEFF_1: float = 1.1
     LEN_STEP: float = 1.38
 
     def __init__(self,
@@ -130,40 +119,39 @@ class Swimming(Training):
 
     def get_spent_calories(self) -> float:
         return ((self.get_mean_speed()
-                + self.coeff_swim_1)
-                * self.coeff_swim_2
-                * self.weight)
-
-    def get_distance(self) -> float:
-        return (self.action
-                * self.LEN_STEP
-                / self.M_IN_KM)
+                + self.SWIM_COEFF_1)
+                * 2 * self.weight)
 
 
-def read_package(workout_type: str, data: list) -> Training:
+def read_package(workout_type: str, data: List[Union[int, float]]) -> Training:
     """Прочитать данные полученные от датчиков."""
-    return TRAINING_TYPE[workout_type](*data)
+    abbreviation_to_training: Dict[str, Type[Training]] = {
+        'SWM': Swimming,
+        'RUN': Running,
+        'WLK': SportsWalking}
+
+    return abbreviation_to_training[workout_type](*data)
 
 
 def main(training: Training) -> None:
     """Главная функция."""
-    info = training.show_training_info()
-    print(info.get_message())
+    print(
+        training
+        .show_training_info()
+        .get_message())
 
 
-TRAINING_TYPE: dict[str, ClassVar] = {'SWM': Swimming,
-                                      'RUN': Running,
-                                      'WLK': SportsWalking,
-                                      }
 if __name__ == '__main__':
     packages = [
         ('SWM', [720, 1, 80, 25, 40]),
         ('RUN', [15000, 1, 75]),
         ('WLK', [9000, 1, 75, 180]),
     ]
+
     for workout_type, data in packages:
-        if workout_type not in TRAINING_TYPE:
-            raise KeyError(
-                f'Тренировка типа "{workout_type}" не поддерживается')
-        training = read_package(workout_type, data)
-        main(training)
+        try:
+            training = read_package(workout_type, data)
+        except KeyError:
+            print('Не определен тип тренировки')
+        else:
+            main(training)
